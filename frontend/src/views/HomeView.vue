7226d28423391af1410286ec57a48f6398e32f8c
n1zen <thisisnez1.2021@gmail.com>
2026-03-06 16:51:54 +0800

    frontend additions, see all products on home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,14 +2,27 @@
   <div class="home">
     Home Page
   </div>
+  <!-- view of all products -->
+  <div v-for="product in products" :key="product.id">
+    {{ product.name }}
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import getProducts from '@/composables/getProducts.js';
 
 export default {
   name: 'HomeView',
   components: {
+  },
+  setup() {
+    // on mount, show all products on a card
+    const { products, error, load } = getProducts()
+
+    load();
+
+    return { products, error };
   }
 }
 </script>
