292b8a55798502f2eb249292606603d3d9a83a8b
n1zen <thisisnez1.2021@gmail.com>
2026-03-06 15:53:00 +0800

    change endpoints to use database. add dependencies for helper functions

---
+++ backend/dependencies.py
@@ -0,0 +1,28 @@
+# some reusable helper functions
+
+from fastapi import HTTPException, status
+from sqlalchemy import select
+
+import models
+
+# check if the item already exists in the database
+# throw an exception if it does
+def does_exist(item: str, content: str, db):
+    if item == "sku":
+        result = db.execute(
+            select(models.Product).where(models.Product.sku == content)
+        )
+        message = "Product SKU already exists."
+    elif item == "name":
+        result = db.execute(
+            select(models.Product).where(models.Product.name == content)
+        )
+        message = "Product NAME already exists."
+    
+    existing_item = result.scalars().first()
+
+    if existing_item:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=message
+        )
--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,23 @@
 # FastAPI backend app
+from typing import Annotated
 
-from fastapi import FastAPI, Request, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+import models
+from database import Base, engine, get_db
 from schemas import(
     ProductCreate,
-    ProductResponse
+    ProductResponse,
+    InventoryItemCreate,
+    InventoryItemResponse
 )
+from dependencies import does_exist
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-products: list[dict] = [
-    {
-        "id": 1,
-        "sku": 250001,
-        "name": "Cat in Box",
-        "category": "Animals",
-        "price": 100,
-    },
-    {
-        "id": 2,
-        "sku": 240005,
-        "name": "Base 1-slot",
-        "category": "Bases",
-        "price": 70,
-    },
-]
 
 @app.get("/")
 def home():
@@ -35,29 +28,42 @@ def home():
     response_model=ProductResponse,
     status_code=status.HTTP_201_CREATED,
 )
-def create_product(product: ProductCreate):
-    new_id = max(p["id"] for p in products) + 1 if products else 1
-    new_product = {
-        "id": new_id,
-        "sku": product.sku,
-        "name": product.name,
-        "category": product.category,
-        "price": product.price
-    }
-    products.append(new_product)
+def create_product(product: ProductCreate, db: Annotated[Session, Depends(get_db)]):
+    
+    does_exist("sku", product.sku, db)
+    does_exist("name", product.name, db)
+    
+    new_product = models.Product(
+        sku = product.sku,
+        name = product.name,
+        category = product.category,
+        price = product.price
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    
     return new_product
 
 # get all products
 @app.get("/api/products", response_model=list[ProductResponse])
-def get_all_products():
+def get_all_products(db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(models.Product)
+    )
+    products = result.scalars().all()
     return products
 
 # get product by id
 @app.get("/api/products/id/{product_id}", response_model=ProductResponse)
-def get_product_by_ID(product_id: int):
-    for product in products:
-        if product.get("id") == product_id:
-            return product
+def get_product_by_ID(product_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(models.Product).where(models.Product.id == product_id)
+    )
+    product = result.scalars().first()
+    if product:
+        return product
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Product not found"
@@ -65,10 +71,13 @@ def get_product_by_ID(product_id: int):
 
 # get product by sku
 @app.get("/api/products/sku/{product_sku}", response_model=ProductResponse)
-def get_product_by_SKU(product_sku: int):
-    for product in products:
-        if product.get("sku") == product_sku:
-            return product
+def get_product_by_SKU(product_sku: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(models.Product).where(models.Product.sku == product_sku)
+    )
+    product = result.scalars().first()
+    if product:
+        return product
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Product not found"
