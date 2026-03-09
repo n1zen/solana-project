e15b4d7b73cb50a60153db1f420e0b8f7d200cb5
n1zen <thisisnez1.2021@gmail.com>
2026-03-09 17:23:33 +0800

    few header changes, connect the route for ProductsView

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,13 +1,19 @@
 <template>
     <header>
-        <h1>Solana Manager</h1>
-        <nav>
-            <router-link to="/">Home</router-link>
-            <router-link to="/products">Products</router-link>
-        </nav>
-        <button>
-            <MoonIcon/>
-        </button>
+        <div class="logo-container">
+            <h1>Solana Manager</h1>
+        </div>
+        <div class="nav-container">
+            <nav>
+                <router-link to="/">Home</router-link>
+                <router-link to="/products">Products</router-link>
+            </nav>
+            <button>
+                <MoonIcon
+                    color="var(--text)"
+                />
+            </button>
+        </div>
     </header>
 </template>
 
@@ -21,4 +27,40 @@ export default {
 </script>
 
 <style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        margin: 0 var(--space-12);
+        padding-top: var(--space-2);
+    }
+    h1 {
+        font-size: var(--text-xl)
+    }
+    .nav-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    nav a {
+        text-decoration: none;
+        color: var(--text);
+        margin: 0 var(--space-4);
+    }
+    nav a:hover {
+        color: var(--primary);
+    }
+    button {
+        background: var(--bg-light);
+        cursor: pointer;
+        padding: var(--space-2) var(--space-3);
+        border-radius: var(--radius-2xl);
+        border: var(--border-card);
+        box-shadow: var(--shadow);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    button:hover {
+        background: var(--gradient);
+    }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductsView from '@/views/ProductsView.vue'
 
 const routes = [
   {
@@ -7,6 +8,11 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/products',
+    name: 'products',
+    component: ProductsView
+  }
 ]
 
 const router = createRouter({
