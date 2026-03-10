088ce16fe403bda764a3018df2cf429b3a5e4f65
n1zen <thisisnez1.2021@gmail.com>
2026-03-10 16:35:38 +0800

    create table to view inventory, create composable to format data and currency

---
+++ frontend/src/components/InventoryTable.vue
@@ -0,0 +1,38 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>SKU</th>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Date Updated</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-if="inventory.length === 0">
+                <td colspan="4">No Products Found</td>
+            </tr>
+            <tr v-for="inventory_item in inventory" :key="inventory_item.id">
+                <td>{{ inventory_item.product.sku }}</td>
+                <td>{{ inventory_item.product.name }}</td>
+                <td>{{ inventory_item.quantity }}</td>
+                <td>{{ formatDate(inventory_item.date_updated) }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import { useFormat } from '@/composables/useFormat.js'
+export default {
+    
+    props: ['inventory'],
+    setup() {
+        const { formatDate } = useFormat();
+        return { formatDate }
+    }
+}
+</script>
+
+<style scoped>
+</style>
